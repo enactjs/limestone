895c36c2d5bb8ed519daeece9e79ccde2011fc17
Daniel Stoian <daniel.stoian@lgepartner.com>
2025-02-07 14:38:17 +0200

    fixed lint warnings

--- Slider/SliderBehaviorDecorator.js
+++ Slider/SliderBehaviorDecorator.js
@@ -39,67 +39,67 @@ const sliderBehaviorDefaultProps = {
 // * Managing focused state to show/hide tooltip
 const SliderBehaviorDecorator = hoc(defaultConfig, (config, Wrapped) => {
 	const {emitSpotlightEvents} = config;
-	
+
 	// eslint-disable-next-line no-shadow
 	const SliderBehaviorDecorator = (props) => {
 		const sliderBehaviorProps = setDefaultProps(props, sliderBehaviorDefaultProps);
-		
+
 		const paused = useMemo(() => new Pause(), []);
 		const [active, setActive] = useState(false);
 		const [dragging, setDragging] = useState(false);
 		const [focused, setFocused] = useState(false);
 		const [useHintText, setUseHintText] = useState(true);
 		const [prevValue, setPrevValue] = useState(sliderBehaviorProps.value);
-		
+
 		useEffect(() => {
 			if (sliderBehaviorProps.value !== prevValue) {
 				setUseHintText(false);
 				setPrevValue(sliderBehaviorProps.value);
 			}
 		}, [prevValue, sliderBehaviorProps.value]);
-		
-		
+
+
 		useEffect(() => {
 			return () => {
 				paused.resume();
 			};
 		}, []); // eslint-disable-line react-hooks/exhaustive-deps
-		
+
 		const getValueText = useCallback(() => {
 			const {'aria-valuetext': ariaValueText, max, min, orientation, value = min} = sliderBehaviorProps;
-			
+
 			const valueText = (ariaValueText != null) ? ariaValueText : value;
 			const verticalHint = `${new IString($L('From {startValue} to {lastValue}')).format({startValue: min, lastValue: max})} ${valueText} ${$L('change a value with up down button')}`;
 			const horizontalHint = `${new IString($L('From {startValue} to {lastValue}')).format({startValue: min, lastValue: max})} ${valueText} ${$L('change a value with left right button')}`;
-			
+
 			if (useHintText) {
 				return orientation === 'horizontal' ? horizontalHint : verticalHint;
 			}
-			
+
 			return valueText;
 		}, [sliderBehaviorProps, useHintText]);
-		
+
 		const handleActivate = useCallback(() => {
 			forwardCustom('onActivate')(null, sliderBehaviorProps);
 			setActive(prevState => !prevState);
 		}, [sliderBehaviorProps]);
-		
+
 		const handleBlur = useCallback((ev) => {
 			forward('onBlur', ev, sliderBehaviorProps);
 			setFocused(false);
 			setUseHintText(true);
 		}, [sliderBehaviorProps]);
-		
+
 		const handleDragStart = useCallback(() => {
 			paused.pause();
 			setDragging(true);
 		}, [paused]);
-		
+
 		const handleDragEnd = useCallback(() => {
 			paused.resume();
 			setDragging(false);
 		}, [paused]);
-		
+
 		const handleFocus = useCallback((ev) => {
 			forward('onFocus', ev, sliderBehaviorProps);
 			if (!sliderBehaviorProps.activateOnSelect) {
@@ -107,28 +107,28 @@ const SliderBehaviorDecorator = hoc(defaultConfig, (config, Wrapped) => {
 			}
 			setFocused(true);
 		}, [handleActivate, sliderBehaviorProps]);
-		
+
 		const handleSpotlightEvents = useCallback((ev) => {
 			if (!emitSpotlightEvents) {
 				forward('onKeyDown', ev, sliderBehaviorProps);
 			}
 			forwardSpotlightEvents(ev, sliderBehaviorProps);
 		}, [sliderBehaviorProps]);
-		
+
 		const sliderProps = Object.assign({}, sliderBehaviorProps);
-		
+
 		if (!emitSpotlightEvents) {
 			// Remove spotlight props before hitting spottable since we've handled them uniquely
 			delete sliderProps.onSpotlightLeft;
 			delete sliderProps.onSpotlightRight;
 			delete sliderProps.onSpotlightUp;
 			delete sliderProps.onSpotlightDown;
-			
+
 			sliderProps.onKeyDown = handleSpotlightEvents;
 		} else {
 			sliderProps[emitSpotlightEvents] = handleSpotlightEvents;
 		}
-		
+
 		return (
 			<Wrapped
 				role="slider"
@@ -144,9 +144,9 @@ const SliderBehaviorDecorator = hoc(defaultConfig, (config, Wrapped) => {
 			/>
 		);
 	};
-	
+
 	SliderBehaviorDecorator.displayName = 'SliderBehaviorDecorator';
-	
+
 	SliderBehaviorDecorator.propTypes = {
 		activateOnSelect: PropTypes.bool,
 		'aria-valuetext': PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
@@ -155,7 +155,7 @@ const SliderBehaviorDecorator = hoc(defaultConfig, (config, Wrapped) => {
 		orientation: PropTypes.string,
 		value: PropTypes.number
 	};
-	
+
 	return SliderBehaviorDecorator;
 });
 
