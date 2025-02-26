d85b24a3ea48d00f08a6230d8ef132cb0d6f8824
Daniel Stoian <daniel.stoian@lgepartner.com>
2025-02-26 14:32:36 +0200

    revert ss tests specs

---
+++ tests/screenshot/specs/light/Light8-specs.js
@@ -0,0 +1,11 @@
+const {runTest} = require('@enact/ui-test-utils/utils');
+
+const Page = require('./LimestonePage');
+
+runTest({
+	testName: 'Limestone Light',
+	Page: Page,
+	skin: 'light',
+	highContrast: false,
+	concurrency: 8
+});
---
+++ tests/screenshot/specs/neutral/HighContrast7-specs.js
@@ -0,0 +1,11 @@
+const {runTest} = require('@enact/ui-test-utils/utils');
+
+const Page = require('./LimestonePage');
+
+runTest({
+	testName: 'Limestone High Contrast',
+	Page: Page,
+	skin: 'neutral',
+	highContrast: true,
+	concurrency: 7
+});
