356c95f84f7ebf02b9d59aafd2f8cf592fd8906e
Daniel Stoian <daniel.stoian@lgepartner.com>
2025-03-21 12:44:28 +0200

    adapted spinner design

--- Spinner/Spinner.js
+++ Spinner/Spinner.js
@@ -59,13 +59,7 @@ const SpinnerCore = kind({
 	render: ({children, css, ...rest}) => (
 		<div aria-live="off" role="alert" {...rest}>
 			<div className={css.bg}>
-				<div className={css.decorator}>
-					<div className={css.fan1} />
-					<div className={css.fan2} />
-					<div className={css.fan3} />
-					<div className={css.fan4} />
-					<div className={css.cap} />
-				</div>
+				<div className={css.decorator} />
 			</div>
 			{children ?
 				<Marquee className={css.client} marqueeOn="render" alignment="center">
