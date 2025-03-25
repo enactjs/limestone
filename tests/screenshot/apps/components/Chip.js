f2d7c7c6f1568b30b5d7e9f6b8973302f7a07eb2
myelyn.kim <myelyn.kim@lge.com>
2025-03-25 09:54:00 +0900

    Added screenshot tests for Chip
    Enact-DCO-1.0-Signed-off-by: Hyelyn Kim (myelyn.kim@lge.com)

---
+++ tests/screenshot/apps/components/Chip.js
@@ -0,0 +1,34 @@
+import Chip from './Chip';
+import {withConfig} from './utils';
+
+const ChipTests = [
+    <Chip />,
+    <Chip>chip</Chip>,
+
+    <Chip icon="check">chip</Chip>,
+
+    <Chip deleteButton>chip</Chip>,
+    <Chip deleteButton={false}>chip</Chip>,
+
+    <Chip deleteButton={{icon: 'closex', position: 'right'}}>chip</Chip>,
+    <Chip deleteButton={{icon: 'closex', position: 'top'}}>chip</Chip>,
+    <Chip deleteButton={{icon: 'closex', position: 'bottom'}}>chip</Chip>,
+
+    <Chip disabled>chip</Chip>,
+    <Chip icon="check" disabled>chip</Chip>,
+    <Chip deleteButton={{icon: 'closex', position: 'right'}} disabled>chip</Chip>,
+
+    <Chip icon="check" deleteButton={{icon: 'delete', position: 'right'}}>chip</Chip>,
+    <Chip icon="check" deleteButton={{icon: 'delete', position: 'top'}}>chip</Chip>,
+    <Chip icon="check" deleteButton={{icon: 'delete', position: 'bottom'}} disabled>chip</Chip>,
+
+     // Focused
+     ...withConfig({focus: true}, [
+        <Chip>chip</Chip>,
+        <Chip icon="check">chip</Chip>,
+        <Chip deleteButton={{icon: 'closex', position: 'right'}}>chip</Chip>,
+        <Chip icon="check" deleteButton={{icon: 'delete', position: 'right'}}>chip</Chip>
+    ])
+];
+
+export default ChipTests;
