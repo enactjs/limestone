0f2c9393684821295dc1de2f4a741e2abfd9bdab
Daniel Stoian <daniel.stoian@lgepartner.com>
2025-03-26 17:52:05 +0200

    adapted limestone ui tests for esm

--- tests/ui/config/wdio.docker.conf.js
+++ tests/ui/config/wdio.docker.conf.js
@@ -1,3 +1,3 @@
-import {config} from '@enact/ui-test-utils/ui/wdio.docker.conf.js';
+import {uiDockerconfig} from '@enact/ui-test-utils/ui/wdio.docker.conf.js';
 
-export default config;
+export default uiDockerconfig;
