c079000ff6d7f0c6a66be21c96f440d5dc6c5f49
Juwon Jeong <juwon.jeong@lge.com>
2025-04-02 14:07:09 +0900

    Fix screenshot test
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)

--- tests/screenshot/apps/components/Card.js
+++ tests/screenshot/apps/components/Card.js
@@ -1,44 +1,40 @@
-import ri from '@enact/ui/resolution';
-
 import Card from '../../../../Card';
 
 import {withConfig, withProps} from './utils';
 
 import img from '../../images/600x600.png';
 
-const verticalStyle = {height: ri.scale(708), width: ri.scale(768)};
-const horizontalStyle = {height: ri.scale(336), width: ri.scale(1500)};
-
+import css from './Card.module.less';
 const defaultCardTests = [
 	// Vertical
-	<Card src={img} style={verticalStyle} orientation="vertical">Short</Card>,
-	<Card src={img} style={verticalStyle} selected>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short">Short</Card>,
-	<Card src={img} style={verticalStyle} label="This is very very very very long label. This is very very long label.">This is very very very very long caption. This is very very long caption.</Card>,
-	<Card src={img} style={verticalStyle} label="Short" selected>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" roudedImage>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" roudedImage selected>Short</Card>,
-	<Card src={img} style={verticalStyle} imageIconSrc={img}>Short</Card>,
-	<Card src={img} style={verticalStyle} imageIconSrc={img} selected>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img}>Short</Card>,
-	<Card src={img} style={verticalStyle} label="This is very very very very long label. This is very very long label." imageIconSrc={img}>This is very very very very long caption. This is very very long caption.</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} selected>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} roundedImage>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} roundedImage selected>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} hasContainer>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} hasContainer roundedImage>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} hasContainer selected>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} captionOverlay>Short</Card>,
-	<Card src={img} style={verticalStyle} label="Short" imageIconSrc={img} captionOverlay selected>Short</Card>,
+	<Card src={img} css={css} orientation="vertical">Short</Card>,
+	<Card src={img} css={css} selected>Short</Card>,
+	<Card src={img} css={css} label="Short">Short</Card>,
+	<Card src={img} css={css} label="This is very very very very long label. This is very very long label.">This is very very very very long caption. This is very very long caption.</Card>,
+	<Card src={img} css={css} label="Short" selected>Short</Card>,
+	<Card src={img} css={css} label="Short" roudedImage>Short</Card>,
+	<Card src={img} css={css} label="Short" roudedImage selected>Short</Card>,
+	<Card src={img} css={css} imageIconSrc={img}>Short</Card>,
+	<Card src={img} css={css} imageIconSrc={img} selected>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img}>Short</Card>,
+	<Card src={img} css={css} label="This is very very very very long label. This is very very long label." imageIconSrc={img}>This is very very very very long caption. This is very very long caption.</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} selected>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} roundedImage>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} roundedImage selected>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} hasContainer>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} hasContainer roundedImage>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} hasContainer selected>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} captionOverlay>Short</Card>,
+	<Card src={img} css={css} label="Short" imageIconSrc={img} captionOverlay selected>Short</Card>,
 
 	// Horizontal
-	<Card src={img} style={horizontalStyle} orientation="horizontal">Short</Card>,
-	<Card src={img} style={horizontalStyle} orientation="horizontal" selected>Short</Card>,
-	<Card src={img} style={horizontalStyle} orientation="horizontal" label="Short">Short</Card>,
-	<Card src={img} style={horizontalStyle} orientation="horizontal" label="This is very very very very long label. This is very very long label.">This is very very very very long caption. This is very very long caption.</Card>,
-	<Card src={img} style={horizontalStyle} orientation="horizontal" label="short" selected>Short</Card>,
-	<Card src={img} style={horizontalStyle} orientation="horizontal" label="Short" roundedImage>Short</Card>,
-	<Card src={img} style={horizontalStyle} orientation="horizontal" label="Short" roundedImage selected>Short</Card>
+	<Card src={img} css={css} orientation="horizontal">Short</Card>,
+	<Card src={img} css={css} orientation="horizontal" selected>Short</Card>,
+	<Card src={img} css={css} orientation="horizontal" label="Short">Short</Card>,
+	<Card src={img} css={css} orientation="horizontal" label="This is very very very very long label. This is very very long label.">This is very very very very long caption. This is very very long caption.</Card>,
+	<Card src={img} css={css} orientation="horizontal" label="short" selected>Short</Card>,
+	<Card src={img} css={css} orientation="horizontal" label="Short" roundedImage>Short</Card>,
+	<Card src={img} css={css} orientation="horizontal" label="Short" roundedImage selected>Short</Card>
 ];
 
 const CardTests = [
