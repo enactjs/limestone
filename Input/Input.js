a8b27f2eadeb56c87b25be2678bdced7781d3b51
alexandru-morariu-lgp <alexandru.morariu@lgepartner.com>
2025-05-09 14:25:13 +0300

    Review fixes.

--- Input/Input.js
+++ Input/Input.js
@@ -386,7 +386,7 @@ const InputPopupBase = kind({
 				icon="arrowhookleft"
 				iconFlip="auto"
 				onClick={onClose}
-				size="small"
+				size="large"
 			/>
 		) : null);
 		const heading = <Heading id={`${id}_title`} size="title" marqueeOn="render" alignment="center" className={css.title}>{title}</Heading>;
