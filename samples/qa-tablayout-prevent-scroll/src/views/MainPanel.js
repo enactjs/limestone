3cd7cefa3dbb74eed12d2f9eabe8cbfe662545e8
Ion Andrusciac <ion.andrusciac@lgepartner.com>
2025-05-21 12:12:29 +0300

    Fixed lint warnings

--- samples/qa-tablayout-prevent-scroll/src/views/MainPanel.js
+++ samples/qa-tablayout-prevent-scroll/src/views/MainPanel.js
@@ -1,37 +1,49 @@
-import React, {useState} from 'react';
-import TabLayout, {Tab} from '@enact/limestone/TabLayout';
+import {useCallback, useState} from 'react';
+import {Panel, Header} from '@enact/limestone/Panels';
+import {TabLayout, Tab} from '@enact/limestone/TabLayout';
+import Item from '@enact/limestone/Item';
 
 const list = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen'];
 
-function MainPanel() {
+function MainPanel () {
 	const [direction, setDirection] = useState('horizontal');
 	const [dynamicList, setDynamicList] = useState(list);
+
+	const handleChangeDirection = useCallback(() => {
+		setDirection(direction === 'horizontal' ? 'vertical' : 'horizontal');
+	}, [direction]);
+
+	const handleAddList = useCallback(() => {
+		const newList = [...dynamicList];
+		newList.push('Added Tab');
+		setDynamicList(newList);
+	}, [dynamicList]);
 
 	const tabList = () => {
 		return dynamicList.map((tab) => {
 			return (
 				<Tab key={tab} title={tab}>
 					<Header title={tab} />
-					<Item onClick={() => {
-						setDirection(direction === 'horizontal' ? 'vertical' : 'horizontal');
-					}}>Change Direction</Item>
-					<Item onClick={() => {
-						const newList = [...dynamicList];
-						newList.push('Added Tab');
-						setDynamicList(newList);
-					}}>
+					<Item
+						onClick={handleChangeDirection}
+					>
+						Change Direction
+					</Item>
+					<Item
+						onClick={handleAddList}
+					>
 						Add List
 					</Item>
 				</Tab>
 			);
 		});
-	}
+	};
 
 	return (
 		<Panel>
 			<TabLayout
 				orientation={direction}
-				style={{ height: '100%', width: '100%' }}
+				style={{height: '100%', width: '100%'}}
 			>
 				{tabList()}
 			</TabLayout>
