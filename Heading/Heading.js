f5c9b79f565106b9bfddbdbcb95f5765e7296ffe
Mikyung Kim <mikyung27.kim@lge.com>
2025-05-23 10:20:48 +0900

    Fixed margin for other size of heading except tiny and color for picker title
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- Heading/Heading.js
+++ Heading/Heading.js
@@ -41,6 +41,27 @@ const HeadingBase = kind({
 	name: 'Heading',
 
 	propTypes: /** @lends limestone/Heading.HeadingBase.prototype */ {
+		/**
+		 * Customizes the component by mapping the supplied collection of CSS class names to the
+		 * corresponding internal elements and states of this component.
+		 *
+		 * The following classes are supported:
+		 *
+		 * * `heading` - The root component class
+		 * * `title` - Applied when `size` is set to "title"
+		 * * `subtitle` - Applied when `size` is set to "subtitle"
+		 * * `large` - Applied when `size` is set to "large"
+		 * * `medium` - Applied when `size` is set to "medium"
+		 * * `small` - Applied when `size` is set to "small"
+		 * * `tiny` - Applied when `size` is set to "tiny"
+		 * * `largeSpacing` - Applied when `spacing` is set to "large"
+		 * * `mediumSpacing` - Applied when `spacing` is set to "medium"
+		 * * `smallSpacing` - Applied when `spacing` is set to "small"
+		 * * `noneSpacing` - Applied when `spacing` is set to "none"
+		 *
+		 * @type {Object}
+		 * @public
+		 */
 		css: PropTypes.object,
 
 		/**
@@ -96,7 +117,8 @@ const HeadingBase = kind({
 
 	styles: {
 		css: componentCss,
-		className: 'heading'
+		className: 'heading',
+		publicClassNames: true
 	},
 
 	computed: {
