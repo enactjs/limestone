a9a13894c0681720a9959d3d4539d5b401d0fa3d
alexandru-morariu-lgp <alexandru.morariu@lgepartner.com>
2025-07-18 10:29:58 +0300

    Changes made to virtualGridList to apply focus on items after they render.

--- VirtualList/useEvent.js
+++ VirtualList/useEvent.js
@@ -268,7 +268,7 @@ const useEventFocus = (props, instances, context) => {
 	const {scrollContainerRef, scrollContentHandle} = instances;
 	const {removeScaleEffect} = context;
 
-	useLayoutEffect(() => {
+	useEffect(() => {
 		function handleFocus (ev) {
 			// only for VirtualGridList
 			// To make the focused item cover other near items
