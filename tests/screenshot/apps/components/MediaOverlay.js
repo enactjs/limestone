a19e942236285a9ea2b8f3985472d4635d86f19d
Daniel Stoian <daniel.stoian@lgepartner.com>
2025-07-22 15:47:53 +0300

    updated mediaplayer ss tests and added scale transform also for imageOverlay

--- tests/screenshot/apps/components/MediaOverlay.js
+++ tests/screenshot/apps/components/MediaOverlay.js
@@ -4,46 +4,55 @@ import img from '../../images/600x600.png';
 
 import {withConfig} from './utils';
 
+const videoSrc="https://media.w3.org/2010/05/sintel/trailer.mp4";
+
 const MediaOverlayTests = [
 	// Base Tests
 	<MediaOverlay />,
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" disabled />,
+	<MediaOverlay caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" disabled><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
+	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
 
 	// Caption / Title / Subtitle combinations
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" />,
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" />,
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" subtitle="07:00 AM - 08:00 AM" />,
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
-	<MediaOverlay imageOverlay={img} title="Program Name" />,
-	<MediaOverlay imageOverlay={img} title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
-	<MediaOverlay imageOverlay={img} subtitle="07:00 AM - 08:00 AM" />,
+	<MediaOverlay caption="DTV 7-1"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay caption="DTV 7-1" title="Program Name"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay caption="DTV 7-1" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay title="Program Name"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
 
-	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start" />,
-	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center" />,
-	<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end" />,
+	<MediaOverlay text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center"><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end"><source src={videoSrc} /></MediaOverlay>,
 
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress />,
-	<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"  />,
+	<MediaOverlay caption="DTV 7-1" progress={0.5} showProgress><source src={videoSrc} /></MediaOverlay>,
+	<MediaOverlay caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
 
 	// Focused
 	...withConfig({focus: true}, [
+		<MediaOverlay caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM" />,
+		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM" disabled><source src={videoSrc} /></MediaOverlay>,
 		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM" disabled />,
 
 		// Caption / Title / Subtitle combinations
-		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" />,
-		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" title="Focused Program Name" />,
-		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" subtitle="07:00 AM - 08:00 AM" />,
-		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM" />,
-		<MediaOverlay imageOverlay={img} title="Focused Program Name" />,
-		<MediaOverlay imageOverlay={img} title="Focused Program Name" subtitle="07:00 AM - 08:00 AM" />,
-		<MediaOverlay imageOverlay={img} subtitle="Focused 07:00 AM - 08:00 AM" />,
+		<MediaOverlay caption="Focused DTV 7-1"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="Focused DTV 7-1" title="Focused Program Name"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="Focused DTV 7-1" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="Focused DTV 7-1" title="Focused Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay title="Focused Program Name"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay title="Focused Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay subtitle="Focused 07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
 
-		<MediaOverlay imageOverlay={img} text="Focused The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start" />,
-		<MediaOverlay imageOverlay={img} text="Focused The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center" />,
-		<MediaOverlay imageOverlay={img} text="Focused The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end" />,
 
-		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" progress={0.5} showProgress />,
-		<MediaOverlay imageOverlay={img} caption="Focused DTV 7-1" progress={0.5} showProgress title="Focused Program Name" subtitle="07:00 AM - 08:00 AM"  />
+		<MediaOverlay text="Focused The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay text="Focused The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay text="Focused The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end"><source src={videoSrc} /></MediaOverlay>,
+
+		<MediaOverlay caption="Focused DTV 7-1" progress={0.5} showProgress><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="Focused DTV 7-1" progress={0.5} showProgress title="Focused Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>
 	]),
 
 	// *************************************************************
@@ -51,22 +60,23 @@ const MediaOverlayTests = [
 	// *************************************************************
 	...withConfig({locale: 'ar-SA'}, [
 		<MediaOverlay />,
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" disabled />,
+		<MediaOverlay caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" disabled><source src={videoSrc} /></MediaOverlay>,
 		// Caption / Title / Subtitle combinations
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" />,
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" />,
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" subtitle="07:00 AM - 08:00 AM" />,
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
-		<MediaOverlay imageOverlay={img} title="Program Name" />,
-		<MediaOverlay imageOverlay={img} title="Program Name" subtitle="07:00 AM - 08:00 AM" />,
-		<MediaOverlay imageOverlay={img} subtitle="07:00 AM - 08:00 AM" />,
+		<MediaOverlay caption="DTV 7-1"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="DTV 7-1" title="Program Name"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="DTV 7-1" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="DTV 7-1" title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay title="Program Name"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>,
 
-		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start" />,
-		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center" />,
-		<MediaOverlay imageOverlay={img} text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end" />,
+		<MediaOverlay text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="start"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="center"><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay text="The quick brown fox jumped over the lazy dog. The bean bird flies at sundown." textAlign="end"><source src={videoSrc} /></MediaOverlay>,
 
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress />,
-		<MediaOverlay imageOverlay={img} caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"  />
+		<MediaOverlay caption="DTV 7-1" progress={0.5} showProgress><source src={videoSrc} /></MediaOverlay>,
+		<MediaOverlay caption="DTV 7-1" progress={0.5} showProgress title="Program Name" subtitle="07:00 AM - 08:00 AM"><source src={videoSrc} /></MediaOverlay>
 	])
 
 ];
