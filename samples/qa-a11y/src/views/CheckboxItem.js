2fd60e981a56dab1cf4bbf546e9fb861ab620683
Jiye Kim <jiye.kim@lge.com>
2025-10-02 14:17:38 +0900

    Updated samples with `CheckboxItemGroup` and `RadioItemGroup` Enact-DCO-1.0-Signed-off-by: Jiye Kim (jiye.kim@lge.com)

--- samples/qa-a11y/src/views/CheckboxItem.js
+++ samples/qa-a11y/src/views/CheckboxItem.js
@@ -1,4 +1,4 @@
-import CheckboxItem from '@enact/limestone/CheckboxItem';
+import CheckboxItem, {CheckboxItemGroup} from '@enact/limestone/CheckboxItem';
 
 import Section from '../components/Section';
 
@@ -14,6 +14,13 @@ const CheckboxItemView = () => (
 		<Section className={appCss.marginTop} title="Aria-labelled">
 			<CheckboxItem alt="Aria-labelled" aria-label="This is a Label 0.">Text 0</CheckboxItem>
 			<CheckboxItem alt="Aria-labelled and Disabled" aria-label="This is a Label 1." disabled>Text 1</CheckboxItem>
+		</Section>
+
+		<Section title="CheckboxItem Group">
+			<CheckboxItemGroup alt="Grouped">
+				<CheckboxItem>Text 0</CheckboxItem>
+				<CheckboxItem disabled>Text 1</CheckboxItem>
+			</CheckboxItemGroup>
 		</Section>
 	</>
 );
