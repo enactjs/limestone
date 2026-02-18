ab9d9bc789cddda23d7e4ca6554de64076b6168d
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-02-18 14:24:56 +0200

    test sample with functional popup

--- samples/qa-dropdown/src/App/App.js
+++ samples/qa-dropdown/src/App/App.js
@@ -1,16 +1,50 @@
-import kind from '@enact/core/kind';
+import * as React from 'react';
+
+import Item from '@enact/limestone/Item';
+import * as Panels from '@enact/limestone/Panels';
+import * as PopupTabLayouts from '@enact/limestone/PopupTabLayout';
+import * as Flexible from '@enact/limestone/FlexiblePopupPanels';
 import ThemeDecorator from '@enact/limestone/ThemeDecorator';
 
-import MainPanel from '../views/MainPanel';
+const MainPanel = props => {
+	const [open, setOpen] = React.useState(false);
 
-const App = kind({
-	name: 'App',
+	const handleBack = React.useCallback(() => {
+		setOpen(false);
+	}, []);
 
-	render: (props) => (
-		<div {...props}>
-			<MainPanel />
-		</div>
-	)
-});
+	const handleOpenClick = React.useCallback(() => {
+		setOpen(true);
+	}, []);
 
-export default ThemeDecorator(App);
+	return (
+		<Panels.Panel>
+			<PopupTabLayouts.PopupTabLayout
+				{...props}
+				open={!open}
+				scrimType="transparent"
+				spotlightRestrict="self-first"
+			>
+				<PopupTabLayouts.Tab title="Tab 1" key="tab1">
+					<PopupTabLayouts.TabPanels onBack={handleBack}>
+						<PopupTabLayouts.TabPanel key="tab1">
+							<Panels.Header title="PopupTabLayout" type="compact" subtitle="This is subtitle" centered />
+							<Item onClick={handleOpenClick}>ABC</Item>
+						</PopupTabLayouts.TabPanel>
+					</PopupTabLayouts.TabPanels>
+				</PopupTabLayouts.Tab>
+			</PopupTabLayouts.PopupTabLayout>
+			<Flexible.FlexiblePopupPanels
+				open={open}
+				noAnimation
+				onClose={handleBack}
+			>
+				<Flexible.Panel size="auto">
+					<Flexible.Header title="Flexible" centered subtitle="abc" />
+				</Flexible.Panel>
+			</Flexible.FlexiblePopupPanels>
+		</Panels.Panel>
+	);
+};
+
+export default ThemeDecorator(MainPanel);
