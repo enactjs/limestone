539c15e6de7b8e8ab2af8178bc6e7b8050a271fd
Jiye Kim <jiye.kim@lge.com>
2026-03-31 18:08:52 +0900

    Fixed lint error Enact-DCO-1.0-Signed-off-by: Jiye Kim (jiye.kim@lge.com)

--- Button/tests/Button-specs.js
+++ Button/tests/Button-specs.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import {render, screen, fireEvent, waitFor} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Button, {ButtonBase} from '../Button';
