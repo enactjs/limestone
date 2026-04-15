62e90ae254a68247c0fc57f346622761ae1e6389
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-04-15 15:54:57 +0300

    code review fixes

--- samples/qa-focus-effect-class/src/App/App.js
+++ samples/qa-focus-effect-class/src/App/App.js
@@ -1,3 +1,4 @@
+import BodyText from '@enact/limestone/BodyText';
 import Button from '@enact/limestone/Button';
 import CheckboxItem from '@enact/limestone/CheckboxItem';
 import {Header, Panel, Panels} from '@enact/limestone/Panels';
@@ -23,24 +24,21 @@ const App = (props) => {
 			<Panel>
 				<Header type="mini">
 					<title>QA Sample - Focus Effect Class</title>
-						<CheckboxItem onClick={onToggleDisabled}>Enable focusEffectClass</CheckboxItem>
+					<CheckboxItem onClick={onToggleDisabled}>Enable focusEffectClass</CheckboxItem>
 					<hr />
 				</Header>
 
 				<Row>
+					<Button className={css.button2} size="large">
+						Button with general focus effect class
+					</Button>
+					<BodyText className={css.bodyText}>When focusEffectClass is active, this Button should have larger font size on focus</BodyText>
+				</Row>
+				<Row>
 					<Button className={css.button1} size="large">
 						Button with data-spotlight-focused attribute
 					</Button>
-				</Row>
-				<Row>
-					<Button className={css.button2} size="large">
-						Button with general focus effect class
-					</Button>
-				</Row>
-				<Row>
-					<Button className={css.button2} size="large">
-						Button with general focus effect class
-					</Button>
+					<BodyText className={css.bodyText}>This Button should have dedicated red color on focus. When focusEffectClass is active, it should have larger font-size</BodyText>
 				</Row>
 			</Panel>
 		</Panels>
