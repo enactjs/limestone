e7719189d63b0ab3fe0690f9fd51ba409904f24a
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-05-04 15:49:31 +0300

    fix for item styling and adding new screenshot tests

--- tests/screenshot/apps/components/SwitchItem.js
+++ tests/screenshot/apps/components/SwitchItem.js
@@ -52,6 +52,23 @@ const SwitchItemTests = [
 		// Centered
 		<SwitchItem centered>Focused Hello SwitchItem</SwitchItem>,
 		<SwitchItem centered>Focused Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam dapibus imperdiet. Morbi diam ex, vulputate eget luctus eu, gravida at ligula. Sed tristique eros sit amet iaculis varius. Phasellus rutrum augue id nulla consectetur, a vulputate velit dictum. Vestibulum ultrices tellus ac cursus condimentum. Aliquam sit amet consectetur nulla, viverra bibendum metus.</SwitchItem>
+	]),
+
+	...withConfig({textSize: 'large'}, [
+		<SwitchItem />,
+		<SwitchItem>Hello SwitchItem</SwitchItem>,
+		<SwitchItem selected>Hello SwitchItem</SwitchItem>,
+		<SwitchItem disabled>Hello SwitchItem</SwitchItem>,
+		<SwitchItem disabled selected>Hello SwitchItem</SwitchItem>,
+		<SwitchItem inline>Hello SwitchItem</SwitchItem>,
+		<SwitchItem inline selected>Hello SwitchItem</SwitchItem>,
+
+		// Icon slotAfter
+		<SwitchItem>SwitchItem<Icon slot="slotAfter">home</Icon></SwitchItem>,
+
+		// Centered
+		<SwitchItem centered>Hello SwitchItem</SwitchItem>,
+		<SwitchItem centered>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam dapibus imperdiet. Morbi diam ex, vulputate eget luctus eu, gravida at ligula. Sed tristique eros sit amet iaculis varius. Phasellus rutrum augue id nulla consectetur, a vulputate velit dictum. Vestibulum ultrices tellus ac cursus condimentum. Aliquam sit amet consectetur nulla, viverra bibendum metus.</SwitchItem>
 	])
 ];
 
