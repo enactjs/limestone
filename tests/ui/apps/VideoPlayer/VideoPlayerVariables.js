23314a9f0bc047c48cc78e5a43916bb86b580f44
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-05-26 11:53:34 +0300

    copied local video also for ui tests

--- tests/ui/apps/VideoPlayer/VideoPlayerVariables.js
+++ tests/ui/apps/VideoPlayer/VideoPlayerVariables.js
@@ -1,5 +1,3 @@
-// Local fixtures so UI tests don't depend on a remote host. See assets/ATTRIBUTION.md
-// for licensing of the bundled media (Sintel trailer, CC-BY 3.0).
 import posterUrl from './assets/poster.png';
 import videoUrl from './assets/movie_90.mp4';
 const videoTitle = 'Limestone VideoPlayer Sample Video';
--- tests/ui/specs/VideoPlayer/VideoPlayer-specs.js
+++ tests/ui/specs/VideoPlayer/VideoPlayer-specs.js
@@ -157,237 +157,237 @@ describe('VideoPlayer', function () {
 		});
 	});
 
-	// describe('disabled', function () {
-	// 	beforeEach(async function () {
-	// 		await Page.open('Disabled');
-	// 	});
-	//
-	// 	it('should not display controls', async function () {
-	// 		await Page.delay(1000);
-	// 		const mediaControlsOpacity = await videoPlayerDisabled.mediaControlsFrame.getCSSProperty('opacity');
-	//
-	// 		expect(await mediaControlsOpacity.value).toBe(0);
-	// 	});
-	//
-	// 	it('should not display controls on click', async function () {
-	// 		await Page.delay(1000);
-	// 		await videoPlayerDisabled.videoPlayerContainer.click();
-	// 		await Page.delay(250);
-	// 		const mediaControlsOpacity = await videoPlayerDisabled.mediaControlsFrame.getCSSProperty('opacity');
-	//
-	// 		expect(await mediaControlsOpacity.value).toBe(0);
-	// 	});
-	//
-	// 	it('should display poster', async function () {
-	// 		await Page.delay(1000);
-	//
-	// 		expect(await videoPlayerDisabled.video.getAttribute('poster')).toBe('https://media.w3.org/2010/05/sintel/poster.png');
-	// 	});
-	//
-	// 	describe('5-way', function () {
-	// 		it('should not display controls on 5-way down', async function () {
-	// 			await Page.delay(1000);
-	// 			await Page.spotlightDown();
-	//
-	// 			await Page.delay(250);
-	//
-	// 			const mediaControlsOpacity = await videoPlayerDisabled.mediaControlsFrame.getCSSProperty('opacity');
-	// 			expect(await mediaControlsOpacity.value).toBe(0);
-	// 		});
-	// 	});
-	// });
-	//
-	//
-	// describe('spotlightDisabled', function () {
-	// 	beforeEach(async function () {
-	// 		await Page.open('SpotlightDisabled');
-	// 	});
-	//
-	// 	it('should not have the play/pause button focused', async function () {
-	// 		await Page.delay(1000);
-	//
-	// 		expect(await videoPlayerSpotlightDisabled.playButton.isFocused()).not.toBe(true);
-	// 	});
-	//
-	// 	it('should play media on playButton click', async function () {
-	// 		await Page.delay(1000);
-	// 		const initialSliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
-	// 		expect(await initialSliderWidth.value).toBe('0px');
-	//
-	// 		await videoPlayerSpotlightDisabled.playButton.click();
-	// 		await Page.delay(3000);
-	//
-	// 		const sliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
-	// 		expect(await sliderWidth.value).not.toBe('0px');
-	// 	});
-	//
-	// 	it('should skip forwards/backwards on previous/next button click', async function () {
-	// 		await Page.delay(1000);
-	// 		const initialSliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
-	// 		expect(await initialSliderWidth.value).toBe('0px');
-	//
-	// 		await videoPlayerSpotlightDisabled.nextButton.click();
-	//
-	// 		const sliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
-	// 		expect(await sliderWidth.value).not.toBe('0px');
-	//
-	// 		await videoPlayerSpotlightDisabled.previousButton.click();
-	// 		const sliderWidthSecond = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
-	// 		expect(await sliderWidthSecond.value).toBe('0px');
-	// 	});
-	//
-	// 	describe('5-way', function () {
-	// 		it('should not show controls on 5-way down', async function () {
-	// 			await Page.delay(1000);
-	// 			const initialMediaControlsOpacity = await videoPlayerSpotlightDisabled.mediaControlsFrame.getCSSProperty('opacity');
-	//
-	// 			if ((await initialMediaControlsOpacity.value) === 1) {
-	// 				await videoPlayerSpotlightDisabled.videoPlayerContainer.click();
-	// 				await Page.delay(250);
-	// 			}
-	//
-	// 			const mediaControlsOpacity = await videoPlayerSpotlightDisabled.mediaControlsFrame.getCSSProperty('opacity');
-	// 			expect(await mediaControlsOpacity.value).toBe(0);
-	//
-	// 			await Page.spotlightDown();
-	// 			await Page.delay(250);
-	//
-	// 			const mediaControlsOpacitySecond = await videoPlayerSpotlightDisabled.mediaControlsFrame.getCSSProperty('opacity');
-	// 			expect(await mediaControlsOpacitySecond.value).toBe(0);
-	// 		});
-	// 	});
-	// });
-	//
-	// describe('props', function () {
-	// 	beforeEach(async function () {
-	// 		await Page.open('Props');
-	// 	});
-	//
-	// 	it('should hide controls after 3 seconds', async function () {
-	// 		await Page.delay(1000);
-	// 		const initialMediaControlsOpacity = await videoPlayerProps.mediaControlsFrame.getCSSProperty('opacity');
-	// 		expect(await initialMediaControlsOpacity.value).toBe(1);
-	//
-	// 		await Page.delay(3000);
-	// 		const mediaControlsOpacity = await videoPlayerProps.mediaControlsFrame.getCSSProperty('opacity');
-	// 		expect(await mediaControlsOpacity.value).toBe(0);
-	// 	});
-	//
-	// 	it('should jump 10 seconds on next/previous button click', async function () {
-	// 		await Page.delay(1000);
-	// 		expect(await videoPlayerProps.mediaControlsTime.getText()).toBe('00:00');
-	// 		await videoPlayerProps.nextButton.click();
-	//
-	// 		expect(await videoPlayerProps.mediaControlsTime.getText()).toBe('00:10');
-	// 		await videoPlayerProps.previousButton.click();
-	// 		expect(await videoPlayerProps.mediaControlsTime.getText()).toBe('00:00');
-	//
-	// 	});
-	//
-	// 	it('should have title', async function () {
-	// 		await Page.delay(1000);
-	//
-	// 		expect(await videoPlayerProps.title.getText()).toBe('Limestone VideoPlayer Sample Video');
-	// 	});
-	//
-	// 	it('should hide title after 1 second', async function () {
-	// 		const initialTitleOpacity = await videoPlayerProps.titleFrame.getCSSProperty('opacity');
-	// 		expect(await initialTitleOpacity.value).toBe(1);
-	//
-	// 		await Page.delay(2000);
-	// 		const titleOpacity = await videoPlayerProps.titleFrame.getCSSProperty('opacity');
-	// 		expect(await titleOpacity.value).toBe(0);
-	// 	});
-	//
-	// 	describe('5-way', function () {
-	// 		it('should not jump on 5-way right', async function () {
-	// 			await Page.delay(1000);
-	//
-	// 			const mediaControlsOpacity = await videoPlayerProps.mediaControlsFrame.getCSSProperty('opacity');
-	// 			if ((await mediaControlsOpacity.value) === 1) {
-	// 				await Page.backKey();
-	// 				await Page.delay(250);
-	// 			}
-	//
-	// 			await Page.spotlightRight();
-	// 			await Page.delay(250);
-	//
-	// 			const sliderWidth = await videoPlayerProps.slider.getCSSProperty('width');
-	// 			expect(await sliderWidth.value).toBe('0px');
-	// 		});
-	// 	});
-	// });
-	//
-	// describe('props2', function () {
-	// 	beforeEach(async function () {
-	// 		await Page.open('Props2');
-	// 	});
-	//
-	// 	it('should not autoshow controls', async function () {
-	// 		await Page.delay(1000);
-	//
-	// 		expect(await videoPlayerProps2.mediaControlsFrame.isExisting()).toBe(false);
-	// 	});
-	//
-	// 	it('should not display slider controls', async function () {
-	// 		await Page.delay(1000);
-	// 		await Page.spotlightDown();
-	// 		await Page.delay(250);
-	//
-	// 		expect(await videoPlayerProps2.slider.isExisting()).toBe(false);
-	// 	});
-	//
-	// 	it('should have seek functionality disabled', async function () {
-	// 		await Page.delay(1000);
-	// 		await Page.spotlightDown();
-	// 		await videoPlayerProps2.nextButton.click();
-	// 		await Page.delay(250);
-	//
-	// 		expect(await videoPlayerProps2.mediaControlsTime.getText()).toBe('00:00');
-	// 	});
-	//
-	// 	it('should display info component', async function () {
-	// 		await Page.delay(1000);
-	// 		await Page.spotlightDown();
-	// 		await Page.spotlightDown();
-	// 		await Page.delay(250);
-	//
-	// 		expect(await videoPlayerProps2.mediaTitleInfoComponent.isExisting()).toBe(true);
-	// 	});
-	// });
-	//
-	// describe('playback speed', function () {
-	// 	beforeEach(async function () {
-	// 		await Page.open('PlaybackSpeed');
-	// 	});
-	//
-	// 	it('should playback speed to 1 when paused in fastforward mode', async function () {
-	// 		await Page.delay(1000);
-	// 		await Page.spotlightDown();
-	// 		await Page.spotlightDown();
-	// 		await Page.spotlightSelect();
-	// 		await Page.delay(500);
-	// 		await Page.spotlightSelect();
-	//
-	// 		expect(await videoPlayerPlaybackSpeed.video.getAttribute('playbackrate')).toBe('1.25');
-	//
-	// 		await Page.spotlightUp();
-	// 		await Page.spotlightSelect();
-	//
-	// 		expect(await videoPlayerPlaybackSpeed.video.getAttribute('playbackrate')).toBe('1');
-	// 	});
-	//
-	// 	it('should playback speed to 2 when pressed playspeed button', async function () {
-	// 		await Page.delay(1000);
-	// 		await Page.spotlightDown();
-	// 		await Page.spotlightDown();
-	// 		await Page.spotlightSelect();
-	// 		await Page.delay(500);
-	//
-	// 		await Page.spotlightRight();
-	// 		await Page.spotlightSelect();
-	//
-	// 		expect(await videoPlayerPlaybackSpeed.video.getAttribute('playbackRate')).toBe('2');
-	// 	});
-	// });
+	describe('disabled', function () {
+		beforeEach(async function () {
+			await Page.open('Disabled');
+		});
+
+		it('should not display controls', async function () {
+			await Page.delay(1000);
+			const mediaControlsOpacity = await videoPlayerDisabled.mediaControlsFrame.getCSSProperty('opacity');
+
+			expect(await mediaControlsOpacity.value).toBe(0);
+		});
+
+		it('should not display controls on click', async function () {
+			await Page.delay(1000);
+			await videoPlayerDisabled.videoPlayerContainer.click();
+			await Page.delay(250);
+			const mediaControlsOpacity = await videoPlayerDisabled.mediaControlsFrame.getCSSProperty('opacity');
+
+			expect(await mediaControlsOpacity.value).toBe(0);
+		});
+
+		it('should display poster', async function () {
+			await Page.delay(1000);
+
+			expect(await videoPlayerDisabled.video.getAttribute('poster')).toBe('https://media.w3.org/2010/05/sintel/poster.png');
+		});
+
+		describe('5-way', function () {
+			it('should not display controls on 5-way down', async function () {
+				await Page.delay(1000);
+				await Page.spotlightDown();
+
+				await Page.delay(250);
+
+				const mediaControlsOpacity = await videoPlayerDisabled.mediaControlsFrame.getCSSProperty('opacity');
+				expect(await mediaControlsOpacity.value).toBe(0);
+			});
+		});
+	});
+
+
+	describe('spotlightDisabled', function () {
+		beforeEach(async function () {
+			await Page.open('SpotlightDisabled');
+		});
+
+		it('should not have the play/pause button focused', async function () {
+			await Page.delay(1000);
+
+			expect(await videoPlayerSpotlightDisabled.playButton.isFocused()).not.toBe(true);
+		});
+
+		it('should play media on playButton click', async function () {
+			await Page.delay(1000);
+			const initialSliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
+			expect(await initialSliderWidth.value).toBe('0px');
+
+			await videoPlayerSpotlightDisabled.playButton.click();
+			await Page.delay(3000);
+
+			const sliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
+			expect(await sliderWidth.value).not.toBe('0px');
+		});
+
+		it('should skip forwards/backwards on previous/next button click', async function () {
+			await Page.delay(1000);
+			const initialSliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
+			expect(await initialSliderWidth.value).toBe('0px');
+
+			await videoPlayerSpotlightDisabled.nextButton.click();
+
+			const sliderWidth = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
+			expect(await sliderWidth.value).not.toBe('0px');
+
+			await videoPlayerSpotlightDisabled.previousButton.click();
+			const sliderWidthSecond = await videoPlayerSpotlightDisabled.slider.getCSSProperty('width');
+			expect(await sliderWidthSecond.value).toBe('0px');
+		});
+
+		describe('5-way', function () {
+			it('should not show controls on 5-way down', async function () {
+				await Page.delay(1000);
+				const initialMediaControlsOpacity = await videoPlayerSpotlightDisabled.mediaControlsFrame.getCSSProperty('opacity');
+
+				if ((await initialMediaControlsOpacity.value) === 1) {
+					await videoPlayerSpotlightDisabled.videoPlayerContainer.click();
+					await Page.delay(250);
+				}
+
+				const mediaControlsOpacity = await videoPlayerSpotlightDisabled.mediaControlsFrame.getCSSProperty('opacity');
+				expect(await mediaControlsOpacity.value).toBe(0);
+
+				await Page.spotlightDown();
+				await Page.delay(250);
+
+				const mediaControlsOpacitySecond = await videoPlayerSpotlightDisabled.mediaControlsFrame.getCSSProperty('opacity');
+				expect(await mediaControlsOpacitySecond.value).toBe(0);
+			});
+		});
+	});
+
+	describe('props', function () {
+		beforeEach(async function () {
+			await Page.open('Props');
+		});
+
+		it('should hide controls after 3 seconds', async function () {
+			await Page.delay(1000);
+			const initialMediaControlsOpacity = await videoPlayerProps.mediaControlsFrame.getCSSProperty('opacity');
+			expect(await initialMediaControlsOpacity.value).toBe(1);
+
+			await Page.delay(3000);
+			const mediaControlsOpacity = await videoPlayerProps.mediaControlsFrame.getCSSProperty('opacity');
+			expect(await mediaControlsOpacity.value).toBe(0);
+		});
+
+		it('should jump 10 seconds on next/previous button click', async function () {
+			await Page.delay(1000);
+			expect(await videoPlayerProps.mediaControlsTime.getText()).toBe('00:00');
+			await videoPlayerProps.nextButton.click();
+
+			expect(await videoPlayerProps.mediaControlsTime.getText()).toBe('00:10');
+			await videoPlayerProps.previousButton.click();
+			expect(await videoPlayerProps.mediaControlsTime.getText()).toBe('00:00');
+
+		});
+
+		it('should have title', async function () {
+			await Page.delay(1000);
+
+			expect(await videoPlayerProps.title.getText()).toBe('Limestone VideoPlayer Sample Video');
+		});
+
+		it('should hide title after 1 second', async function () {
+			const initialTitleOpacity = await videoPlayerProps.titleFrame.getCSSProperty('opacity');
+			expect(await initialTitleOpacity.value).toBe(1);
+
+			await Page.delay(2000);
+			const titleOpacity = await videoPlayerProps.titleFrame.getCSSProperty('opacity');
+			expect(await titleOpacity.value).toBe(0);
+		});
+
+		describe('5-way', function () {
+			it('should not jump on 5-way right', async function () {
+				await Page.delay(1000);
+
+				const mediaControlsOpacity = await videoPlayerProps.mediaControlsFrame.getCSSProperty('opacity');
+				if ((await mediaControlsOpacity.value) === 1) {
+					await Page.backKey();
+					await Page.delay(250);
+				}
+
+				await Page.spotlightRight();
+				await Page.delay(250);
+
+				const sliderWidth = await videoPlayerProps.slider.getCSSProperty('width');
+				expect(await sliderWidth.value).toBe('0px');
+			});
+		});
+	});
+
+	describe('props2', function () {
+		beforeEach(async function () {
+			await Page.open('Props2');
+		});
+
+		it('should not autoshow controls', async function () {
+			await Page.delay(1000);
+
+			expect(await videoPlayerProps2.mediaControlsFrame.isExisting()).toBe(false);
+		});
+
+		it('should not display slider controls', async function () {
+			await Page.delay(1000);
+			await Page.spotlightDown();
+			await Page.delay(250);
+
+			expect(await videoPlayerProps2.slider.isExisting()).toBe(false);
+		});
+
+		it('should have seek functionality disabled', async function () {
+			await Page.delay(1000);
+			await Page.spotlightDown();
+			await videoPlayerProps2.nextButton.click();
+			await Page.delay(250);
+
+			expect(await videoPlayerProps2.mediaControlsTime.getText()).toBe('00:00');
+		});
+
+		it('should display info component', async function () {
+			await Page.delay(1000);
+			await Page.spotlightDown();
+			await Page.spotlightDown();
+			await Page.delay(250);
+
+			expect(await videoPlayerProps2.mediaTitleInfoComponent.isExisting()).toBe(true);
+		});
+	});
+
+	describe('playback speed', function () {
+		beforeEach(async function () {
+			await Page.open('PlaybackSpeed');
+		});
+
+		it('should playback speed to 1 when paused in fastforward mode', async function () {
+			await Page.delay(1000);
+			await Page.spotlightDown();
+			await Page.spotlightDown();
+			await Page.spotlightSelect();
+			await Page.delay(500);
+			await Page.spotlightSelect();
+
+			expect(await videoPlayerPlaybackSpeed.video.getAttribute('playbackrate')).toBe('1.25');
+
+			await Page.spotlightUp();
+			await Page.spotlightSelect();
+
+			expect(await videoPlayerPlaybackSpeed.video.getAttribute('playbackrate')).toBe('1');
+		});
+
+		it('should playback speed to 2 when pressed playspeed button', async function () {
+			await Page.delay(1000);
+			await Page.spotlightDown();
+			await Page.spotlightDown();
+			await Page.spotlightSelect();
+			await Page.delay(500);
+
+			await Page.spotlightRight();
+			await Page.spotlightSelect();
+
+			expect(await videoPlayerPlaybackSpeed.video.getAttribute('playbackRate')).toBe('2');
+		});
+	});
 });
