23174d853bda7b0719256428c68e9c28d1f3f616
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-05-26 11:45:12 +0300

    copied local video also for ui tests

--- tests/ui/apps/VideoPlayer/VideoPlayerVariables.js
+++ tests/ui/apps/VideoPlayer/VideoPlayerVariables.js
@@ -1,5 +1,7 @@
-const posterUrl = 'https://media.w3.org/2010/05/sintel/poster.png';
-const videoUrl = 'https://media.w3.org/2010/05/sintel/trailer.mp4';
+// Local fixtures so UI tests don't depend on a remote host. See assets/ATTRIBUTION.md
+// for licensing of the bundled media (Sintel trailer, CC-BY 3.0).
+import posterUrl from './assets/poster.png';
+import videoUrl from './assets/movie_90.mp4';
 const videoTitle = 'Limestone VideoPlayer Sample Video';
 
 export {posterUrl, videoUrl, videoTitle};
