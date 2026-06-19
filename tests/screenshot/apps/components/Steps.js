db1f988db663624e9a75a706f9df977803fa2624
alexandru-morariu-lgp <84004128+alexandrumorariu@users.noreply.github.com>
2026-06-19 09:58:31 +0300

    Small fixes

--- tests/screenshot/apps/components/Steps.js
+++ tests/screenshot/apps/components/Steps.js
@@ -43,8 +43,8 @@ const StepsTests = [
 	<Steps current={3} currentIcon="arrowlargeup" futureIcon="arrowlargeright" pastIcon="arrowlargeleft" skip={[1, 5, 10]} total={10} />,
 	<Steps current={3} currentIcon="arrowlargeup" futureIcon="arrowlargeright" pastIcon="arrowlargeleft" skip={[1, 5, 10]} skipIcon="numbers" total={10} />,
 	<Steps currentIcon="arrowlargeup" futureIcon="arrowlargeright" pastIcon="arrowlargeleft" skip={[1, 5, 10]} skipIcon="closex" total={10} />,
-	<Steps current={3} currentIcon="arrowlargeup" futureIcon="arrowlargeright" pastIcon="arrowlargeleft" skip={[1, 5, 10]} skipIcon="closex" total={10} />
+	<Steps current={3} currentIcon="arrowlargeup" futureIcon="arrowlargeright" pastIcon="arrowlargeleft" skip={[1, 5, 10]} skipIcon="closex" total={10} />,
 	<Steps highlightCurrentOnly total={3} />,
-	<Steps highlightCurrentOnly total={25} />,
+	<Steps highlightCurrentOnly total={25} />
 ];
 export default StepsTests;
