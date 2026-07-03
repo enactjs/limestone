f0a0c24c2e06dc504f64bff325d146970ecb522f
daniel-stoian-lgp <daniel.stoian@lgepartner.com>
2026-07-03 10:43:25 +0300

    reverted interactions

--- samples/sampler/stories/default/Button.js
+++ samples/sampler/stories/default/Button.js
@@ -1,5 +1,6 @@
 import Button, {ButtonBase} from '@enact/limestone/Button';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
+import {action} from '@enact/storybook-utils/addons/actions';
 import {boolean, select, text} from '@enact/storybook-utils/addons/controls';
 import UIButton, {ButtonBase as UIButtonBase} from '@enact/ui/Button';
 import {Fragment} from 'react';
