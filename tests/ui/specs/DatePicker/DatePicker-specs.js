936e3e38bfed48b172cc31c0069059b4eb6a76a5
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-09-03 12:31:41 +0300

    Increased tests delay to wait for animation transition

--- tests/ui/specs/DatePicker/DatePicker-specs.js
+++ tests/ui/specs/DatePicker/DatePicker-specs.js
@@ -33,7 +33,7 @@ describe('DatePicker', function () {
 					const {month} = await extractValues(datePicker);
 					expect(await datePicker.month.isFocused()).toBe(true);
 					await Page.spotlightUp();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {month: value} = await extractValues(datePicker);
 					const expected = month < 12 ? month + 1 : 1;
 					expect(value).toBe(expected);
@@ -43,7 +43,7 @@ describe('DatePicker', function () {
 					const {month} = await extractValues(datePicker);
 					expect(await datePicker.month.isFocused()).toBe(true);
 					await Page.spotlightDown();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {month: value} = await extractValues(datePicker);
 					const expected = month > 1 ? month - 1 : 12;
 					expect(value).toBe(expected);
@@ -55,7 +55,7 @@ describe('DatePicker', function () {
 					await Page.spotlightRight();
 					expect(await datePicker.day.isFocused()).toBe(true);
 					await Page.spotlightUp();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {day: value} = await extractValues(datePicker);
 					const expected = day !== numDays ? day + 1 : 1;
 					expect(value).toBe(expected);
@@ -79,7 +79,7 @@ describe('DatePicker', function () {
 					await Page.spotlightRight();
 					expect(await datePicker.year.isFocused()).toBe(true);
 					await Page.spotlightUp();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {year: value} = await extractValues(datePicker);
 					const expected = year + 1;
 					expect(value).toBe(expected);
@@ -91,7 +91,7 @@ describe('DatePicker', function () {
 					await Page.spotlightRight();
 					expect(await datePicker.year.isFocused()).toBe(true);
 					await Page.spotlightDown();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {year: value} = await extractValues(datePicker);
 					const expected = year - 1;
 					expect(value).toBe(expected);
@@ -135,7 +135,7 @@ describe('DatePicker', function () {
 					await Page.delay(200);
 					expect(await datePicker.month.isFocused()).toBe(true);
 					await datePicker.incrementer('month').click();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {month: value} = await extractValues(datePicker);
 					const expected = month < 12 ? month + 1 : 1;
 					expect(value).toBe(expected);
@@ -147,7 +147,7 @@ describe('DatePicker', function () {
 					await Page.delay(200);
 					expect(await datePicker.month.isFocused()).toBe(true);
 					await datePicker.decrementer('month').click();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {month: value} = await extractValues(datePicker);
 					const expected = month > 1 ? month - 1 : 12;
 					expect(value).toBe(expected);
@@ -160,7 +160,7 @@ describe('DatePicker', function () {
 					await Page.delay(500);
 					expect(await datePicker.day.isFocused()).toBe(true);
 					await datePicker.incrementer('day').click();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {day: value} = await extractValues(datePicker);
 					const expected = day !== numDays ? day + 1 : 1;
 					expect(value).toBe(expected);
@@ -185,7 +185,7 @@ describe('DatePicker', function () {
 					await Page.delay(200);
 					expect(await datePicker.year.isFocused()).toBe(true);
 					await datePicker.incrementer('year').click();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {year: value} = await extractValues(datePicker);
 					const expected = year + 1;
 					expect(value).toBe(expected);
@@ -197,7 +197,7 @@ describe('DatePicker', function () {
 					await Page.delay(200);
 					expect(await datePicker.year.isFocused()).toBe(true);
 					await datePicker.decrementer('year').click();
-					await Page.delay(500);
+					await Page.delay(1000);
 					const {year: value} = await extractValues(datePicker);
 					const expected = year - 1;
 					expect(value).toBe(expected);
