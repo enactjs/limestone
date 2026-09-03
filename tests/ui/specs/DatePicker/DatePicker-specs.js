36030e3de733b976c286ca4e224d5f4af245501e
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2026-09-03 16:07:55 +0300

    Increased timeout

--- tests/ui/specs/DatePicker/DatePicker-specs.js
+++ tests/ui/specs/DatePicker/DatePicker-specs.js
@@ -385,15 +385,15 @@ describe('DatePicker', function () {
 			await datePickerCheckMinValue.decrementer('year').click();
 			await datePickerCheckMinValue.decrementer('year').click();
 			// Step 3 Verify: Year Value changes to 1900.
-			await Page.delay(1000);
+			await Page.delay(500);
 			expect((await extractValues(datePickerCheckMinValue)).year).toBe(1900);
 
 			// check disabled picker does not work when value reached minimum end-value.
 			await datePickerCheckMinValue.decrementer('year').click();
-			await Page.delay(1000);
+			await Page.delay(500);
 			expect((await extractValues(datePickerCheckMinValue)).year).toBe(1900);
 			await datePickerCheckMinValue.incrementer('year').click();
-			await Page.delay(1000);
+			await Page.delay(500);
 			expect((await extractValues(datePickerCheckMinValue)).year).toBe(1901);
 		});
 
@@ -405,15 +405,15 @@ describe('DatePicker', function () {
 			await datePickerCheckMaxValue.incrementer('year').click();
 			await datePickerCheckMaxValue.incrementer('year').click();
 			// Step 3 Verify: Year Value changes to 2099.
-			await Page.delay(1000);
+			await Page.delay(500);
 			expect((await extractValues(datePickerCheckMaxValue)).year).toBe(2099);
 
 			// check disabled picker does not work when value reached maximum end-value.
 			await datePickerCheckMaxValue.incrementer('year').click();
-			await Page.delay(1000);
+			await Page.delay(500);
 			expect((await extractValues(datePickerCheckMaxValue)).year).toBe(2099);
 			await datePickerCheckMaxValue.decrementer('year').click();
-			await Page.delay(1000);
+			await Page.delay(500);
 			expect((await extractValues(datePickerCheckMaxValue)).year).toBe(2098);
 		});
 	});
